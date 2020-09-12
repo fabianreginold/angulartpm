73a37b14e4225510d65027fd04b1cfe4827ad2c7
freginol <freginol@FREGINOL-IN.in.oracle.com>
2020-09-12 18:13:36 +0530

    new commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  flag :String="Click me";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clicking()
+  {
+    console.log("its clicked dude");
+    this.flag="Thank you for clicking"
   }
 
 }
